5e66fd23f81c7a544754df456a6d522029b2e7cc
linux <linux@linux.(none)>
2014-07-03 15:11:38 +0800

    Fifth commit

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,25 @@ class Poll(models.Model):
 	return self.question
 
     def was_published_recently(self):
-	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	now = timezone.now()
+    	return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def test_was_published_recently_with_old_poll(self):
+    	"""
+    	was_published_recently() should return False for polls whose pub_date
+    	is older than 1 day
+    	"""
+    	old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+    	self.assertEqual(old_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_poll(self):
+    	"""
+    	was_published_recently() should return True for polls whose pub_date
+    	is within the last day
+    	"""
+    	recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+    	self.assertEqual(recent_poll.was_published_recently(), True)
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
